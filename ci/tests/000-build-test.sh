3669b0e74bffb78cbca16eb296bb699d952f01cb
Yuri Sibirski <yuri@horizenlabs.io>
2023-06-06 18:07:33 -0400

    New CI/CD including mvn commands

---
+++ ci/tests/000-build-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eo pipefail
+
+retval=0
+
+# Building jar package
+echo "" && echo "=== Building jar(s) ===" && echo ""
+mvn clean package -DskipTests=true -B || retval="$?"
+
+# Running mvn tests
+echo "" && echo "=== Running maven build test(s) ===" && echo ""
+mvn test -B || retval="$?"
+
+exit "$retval"
