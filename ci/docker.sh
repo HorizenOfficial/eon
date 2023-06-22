6c38b66309da72c395a7f3dc43248c07353b9a58
otoumas <tomas@horizenlabs.io>
2023-06-22 19:04:55 +0100

    DEVOP-896 Create bootstrap tool docker image and publish it to Dockerhub. Provide a script to generate keys

--- ci/docker.sh
+++ ci/docker.sh
@@ -2,7 +2,8 @@
 set -eEuo pipefail
 
 workdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
-docker_image_name="${DOCKER_IMAGE_NAME:-evmapp}"
+evmapp_docker_image_name="${EVMAPP_DOCKER_IMAGE_NAME:-evmapp}"
+bootstraptool_docker_image_name="${BOOTSTRAPTOOL_DOCKER_IMAGE_NAME:-bootstraptool}"
 docker_hub_org='horizenlabs'
 pom_version="${ROOT_POM_VERSION:-}"
 
@@ -31,11 +32,16 @@ fi
 
 # Building docker image
 if [ -n "${docker_tag}" ]; then
-  echo "" && echo "=== Building Docker Image: ${docker_image_name}:${docker_tag} ===" && echo ""
+  echo "" && echo "=== Building Docker Image: ${evmapp_docker_image_name}:${docker_tag} ===" && echo ""
 
-  docker build -f "${workdir}"/dockerfiles/evmapp/Dockerfile -t "${docker_image_name}:${docker_tag}" \
+  docker build -f "${workdir}"/dockerfiles/evmapp/Dockerfile -t "${evmapp_docker_image_name}:${docker_tag}" \
     --build-arg ARG_SC_COMMITTISH="${arg_sc_committish}" \
     --build-arg ARG_SC_VERSION="${arg_sc_version}" \
+    .
+
+  docker build -f "${workdir}"/dockerfiles/bootstraptool/Dockerfile -t "${bootstraptool_docker_image_name}:${docker_tag}" \
+    --build-arg ARG_SC_VERSION=0.1.0-SNAPSHOT \
+    --build-arg ARG_SC_COMMITTISH=0.1.0-SNAPSHOT9 \
     .
 
   # Publishing to DockerHub
@@ -44,8 +50,15 @@ if [ -n "${docker_tag}" ]; then
     echo "Warning: DOCKER_USERNAME and/or DOCKER_USERNAME is(are) empty. Docker image is NOT going to be published on DockerHub !!!"
   else
     echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-    docker tag "${docker_image_name}:${docker_tag}" "index.docker.io/${docker_hub_org}/${docker_image_name}:${docker_tag}"
-    docker push "index.docker.io/${docker_hub_org}/${docker_image_name}:${docker_tag}"
+    echo "Create tags"
+    docker_images=("${evmapp_docker_image_name}" "${bootstraptool_docker_image_name}")
+    for docker_image in "${docker_images[@]}"; do
+      tags=("${docker_tag}" "latest")
+      for tag in "${tags[@]}"; do
+        docker tag "${docker_image}:${tag}" "index.docker.io/${docker_hub_org}/${docker_image}:${tag}"
+        docker push "index.docker.io/${docker_hub_org}/${docker_image}:${tag}"
+      done
+    done
   fi
 else
   echo "" && echo "=== The build did NOT satisfy RELEASE build requirements. Docker image is not being created ===" && echo ""
