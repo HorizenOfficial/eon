cbbda661c2a7083beba21c0608c9ace039713137
Alberto Sala <albertosala@horizenlabs.io>
2023-06-30 12:21:32 +0200

    Upgrade EON app to confgure the new hardfork for zenDAO

--- node/src/main/java/io/horizen/eon/EonForkConfigurator.java
+++ node/src/main/java/io/horizen/eon/EonForkConfigurator.java
@@ -1,6 +1,7 @@
 package io.horizen.eon;
 
 import io.horizen.account.fork.GasFeeFork;
+import io.horizen.account.fork.ZenDAOFork;
 import io.horizen.fork.ForkConfigurator;
 import io.horizen.fork.OptionalSidechainFork;
 import io.horizen.fork.SidechainForkConsensusEpoch;
@@ -12,22 +13,45 @@ import java.util.Optional;
 
 public class EonForkConfigurator extends ForkConfigurator {
 
+    static final int FEE_REGTEST_FORKPOINT = 0;
+    static final int FEE_PREGOBI_TESTNET_FORKPOINT = 718;
+    static final int FEE_GOBI_TESTNET_FORKPOINT = 624;
+    static final int FEE_TESTNET_FORKPOINT = 0;
+    static final int FEE_MAINNET_FORKPOINT = 0;
+
+    static final int ZENDAO_REGTEST_FORKPOINT = 7;
+    static final int ZENDAO_TESTNET_FORKPOINT = 800;
+    static final int ZENDAO_MAINNET_FORKPOINT = 7;
 
     private final SidechainForkConsensusEpoch mandatorySidechainFork1;
     private final List<Pair<SidechainForkConsensusEpoch, OptionalSidechainFork>> optionalSidechainForks;
 
-    private final GasFeeFork feeFork1Params =  new GasFeeFork(
-            // block gas limit: 10 million
-            BigInteger.valueOf(10000000),
-            BigInteger.valueOf(2),
-            BigInteger.valueOf(8),
-            // minimum base fee: 20 Gwei (20*10^9)
-            BigInteger.valueOf(20000000000L)
-    );
-
     public EonForkConfigurator(Optional<String> sidechainId) {
-        optionalSidechainForks=  List.of(
-                new Pair<>(new SidechainForkConsensusEpoch(0, getFeeForkTestnetActivation(sidechainId), 0), feeFork1Params)
+        // block gas limit: 10 million
+        // minimum base fee: 20 Gwei (20*10^9)
+        GasFeeFork feeFork1Params = new GasFeeFork(
+                // block gas limit: 10 million
+                BigInteger.valueOf(10000000),
+                BigInteger.valueOf(2),
+                BigInteger.valueOf(8),
+                // minimum base fee: 20 Gwei (20*10^9)
+                BigInteger.valueOf(20000000000L)
+        );
+
+        ZenDAOFork zenDAOFork = new ZenDAOFork(false);
+
+        optionalSidechainForks = List.of(
+                new Pair<>(new SidechainForkConsensusEpoch(
+                        FEE_REGTEST_FORKPOINT,
+                        getFeeForkTestnetActivation(sidechainId),
+                        FEE_MAINNET_FORKPOINT),
+                        feeFork1Params),
+
+                new Pair<>(new SidechainForkConsensusEpoch(
+                        ZENDAO_REGTEST_FORKPOINT,
+                        getZenDAOTestnetActivation(sidechainId),
+                        ZENDAO_MAINNET_FORKPOINT),
+                        zenDAOFork)
         );
         mandatorySidechainFork1 = new SidechainForkConsensusEpoch(0, 0, 0);
     }
@@ -37,16 +61,29 @@ public class EonForkConfigurator extends ForkConfigurator {
             switch (sidechainId.get()){
                 case ApplicationConstants.PREGOBI_SIDECHAINID:
                     //Pre-Gobi (parallel testnet) fork configuration
-                    return 718;
+                    return FEE_PREGOBI_TESTNET_FORKPOINT;
                 case ApplicationConstants.GOBI_SIDECHAINID:
                     //Gobi (official testnet) fork configuration
-                    return 624;
+                    return FEE_GOBI_TESTNET_FORKPOINT;
                 default:
                     //any other testnet
-                    return 0;
+                    return FEE_TESTNET_FORKPOINT;
             }
         }else{
             return 0;
+        }
+    }
+
+    private int getZenDAOTestnetActivation(Optional<String> sidechainId){
+        if (sidechainId.isPresent()){
+            switch (sidechainId.get()){
+                // TODO: handle any specific sidechain if any. For the time being there is only one possibility
+                default:
+                    //any other testnet
+                    return ZENDAO_TESTNET_FORKPOINT;
+            }
+        } else {
+            return ZENDAO_TESTNET_FORKPOINT;
         }
     }
 
